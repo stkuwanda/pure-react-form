691e8ce8151c73b8990d52e894fa5b6a7b648feb
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-23 15:49:14 +0200

    used a controlled component for updating the username form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import FormInput from './components/FormInput/FormInput.component';
 
 function App() {
+	const [username, setUsername] = useState('');
+
+	function inputHandler(event) {
+		setUsername(event.target.value);
+	}
+
 	return (
 		<div className='app'>
 			<form>
-        <FormInput placeholder='Username'/>
-        <FormInput placeholder='Email'/>
-        <FormInput placeholder='Full Name'/>
-        <FormInput placeholder='another'/>
-      </form>
+				<FormInput placeholder='Username' value={username} onChange={inputHandler} />
+				<FormInput placeholder='Email' />
+				<FormInput placeholder='Full Name' />
+				<FormInput placeholder='another' />
+			</form>
 		</div>
 	);
 }
