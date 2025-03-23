b699b4345b36275b34bd665264094d0254e76d25
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-23 23:58:35 +0200

    implemented primitive form validation utilizing css classes and input contraint attributes such as required and pattern

--- src/App.jsx
+++ src/App.jsx
@@ -16,48 +16,61 @@ function App() {
 			id: 1,
 			name: 'username',
 			type: 'text',
+			errorMessage:
+				"Username should be 3-16 characters and shouldn't include any special characters!",
 			label: 'Username',
-      placeholder: 'Enter username'
+			placeholder: 'Enter username',
+			pattern: '^[A-Za-z0-9]{3,16}$',
+			required: true
 		},
 		{
 			id: 2,
 			name: 'email',
 			type: 'email',
+			errorMessage: 'Enter a valid email address!',
 			label: 'Email',
-      placeholder: 'Enter email'
+			placeholder: 'Enter email',
+			required: true
 		},
 		{
 			id: 3,
 			name: 'birthday',
 			type: 'date',
 			label: 'Birthday',
-      placeholder: 'Birthday'
+			placeholder: 'Birthday',
 		},
 		{
 			id: 4,
 			name: 'password',
 			type: 'password',
+			errorMessage:
+				'Password should be 8-20 characters and must include at least 1 letter, 1 number and 1 special character!',
 			label: 'Username',
-      placeholder: 'Enter password'
+			placeholder: 'Enter password',
+			pattern: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$',
+			required: true
 		},
 		{
 			id: 5,
 			name: 'confirmPassword',
 			type: 'password',
+			errorMessage: 'Passwords must match!',
 			label: 'Confirm Password',
-      placeholder: 'Enter password'
+			placeholder: 'Enter password',
+			pattern: values.password,
+			required: true
 		},
 	];
 
 	function handleSubmit(event) {
 		event.preventDefault();
-    console.log(values);
+		console.log(values);
 	}
 
 	return (
 		<div className='app'>
 			<form onSubmit={handleSubmit}>
-        <h1>Register</h1>
+				<h1>Register</h1>
 				{inputs.map((input) => (
 					<FormInput
 						key={input.id}
--- src/components/FormInput/FormInput.component.jsx
+++ src/components/FormInput/FormInput.component.jsx
@@ -1,10 +1,11 @@
 import './FormInput.component.css';
 
-function FormInput({ id, label, ...props }) {
+function FormInput({ id, label, errorMessage, ...props }) {
 	return (
 		<div className='formInput'>
-			<label htmlFor={id}>{label}</label> 
+			<label htmlFor={id}>{label}</label>
 			<input id={id} {...props} />
+			<span>{errorMessage}</span>
 		</div>
 	);
 }
