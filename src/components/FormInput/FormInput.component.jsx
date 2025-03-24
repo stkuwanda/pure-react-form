00f0add319cda213599fc6d2b56713bdfb6f004c
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-24 08:26:40 +0200

    improved upon validation UI by adding onBlur, onFocus handlers and corresponding styling

--- src/components/FormInput/FormInput.component.jsx
+++ src/components/FormInput/FormInput.component.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './FormInput.component.css';
 
 function FormInput({ id, label, errorMessage, ...props }) {
+	const [focused, setFocused] = useState(false);
+
+	function handleBlur() {
+		setFocused(true);
+	}
+
+	function handleFocus() {
+		props.name === 'confirmPassword' && setFocused(true);
+	}
+
 	return (
 		<div className='formInput'>
 			<label htmlFor={id}>{label}</label>
-			<input id={id} {...props} />
+			<input id={id} {...props} onBlur={handleBlur} focused={focused.toString()} onFocus={handleFocus}/>
 			<span>{errorMessage}</span>
 		</div>
 	);
